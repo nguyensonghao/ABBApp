b34ae502f8f3bf5798604dd28bf7a9fc1621deed
TuanNguyen <tuan.nguyen@launchdeckpowergate.com>
2017-11-08 17:02:51 +0700

    Create directive commentbox

--- www/components/feeds/index.js
+++
@@ -1,9 +0,0 @@
-var blockchain = require('blockchain-node');
-var bc = new blockchain({
-    username: 'username',
-    password: 'password'
-});
-
-bc.getInfo(function(err, info) {
-	console.log(info);
-});
