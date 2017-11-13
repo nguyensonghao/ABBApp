c50913b006ace1a828ed71a85e38545d1fdf7538
TuanNguyen <tuan.nguyen@launchdeckpowergate.com>
2017-11-13 19:17:49 +0700

    Fix bug

--- www/pages/home/home.js
+++ www/pages/home/home.js
@@ -21,12 +21,13 @@ angular.module('AbbApp').controller('HomeController', ["$rootScope", '$interval'
         { title: 'Phút', time: '0' },
         { title: 'Giây', time: '0' }
       ]
+      vm.countDownStatus = 'screen-last';
     } else {
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+      vm.countDownStatus = 'screen-first';
       vm.countDownTime = [
         { title: 'Ngày', time: days },
         { title: 'Giờ', time: hours },
